08894ae643abbf26a591df0a0bdd901ef74ba9d3
xfd <xfd@xfd.com>
2022-11-06 20:55:36 +0800

    hot-fix commit2

--- src/main/java/com/xfd/HbaseConnection/GitTest.java
+++ src/main/java/com/xfd/HbaseConnection/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello git4!");
         System.out.println("hello git4!");
+        System.out.println("hello Test!");
 
     }
 }
