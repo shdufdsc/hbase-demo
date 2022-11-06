8a47fa194de870247e190af26ec08e5fe6b1970c
xfd <xfd@xfd.com>
2022-11-06 20:57:19 +0800

    master commit2

--- src/main/java/com/xfd/HbaseConnection/GitTest.java
+++ src/main/java/com/xfd/HbaseConnection/GitTest.java
@@ -8,6 +8,8 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello master test!");
+        System.out.println("hello master test!");
 
     }
 }
