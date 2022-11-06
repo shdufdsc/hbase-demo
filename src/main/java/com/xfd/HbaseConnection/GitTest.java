633d4614a5ee9b8d3d64888f78466c89041d0829
xfd <xfd@xfd.com>
2022-11-06 21:37:41 +0800

    push commit2

--- src/main/java/com/xfd/HbaseConnection/GitTest.java
+++ src/main/java/com/xfd/HbaseConnection/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("hello git4!");
         System.out.println("hello Test!");
+        System.out.println("push Test!");
 
     }
 }
