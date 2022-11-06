5939d0b701be371048c690e141490cfb28991b37
Immortal <93002325+shdufdsc@users.noreply.github.com>
2022-11-06 22:33:00 +0800

    giuhub update

--- src/main/java/com/xfd/HbaseConnection/GitTest.java
+++ src/main/java/com/xfd/HbaseConnection/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello Test!");
         System.out.println("push Test!");
         System.out.println("pull Test!");
+        System.out.println("github 东东东东东 龙龙龙龙");
     }
 }
